d458f28c9419d2ed19b2ebaaaf9e57535ef65e18
Nicolas Deman <nicolas.deman@its4u.lu>
2023-04-18 18:26:40 +0200

    Liskov principle refactor

--- 3-SOLID/Java/liskov_substitution/LSP-toCorrect/src/UnitUpgrader.java
+++ 3-SOLID/Java/liskov_substitution/LSP-toCorrect/src/UnitUpgrader.java
@@ -2,10 +2,12 @@
  * Created by mrk on 4/8/14.
  */
 public class UnitUpgrader {
-    public void upgrade(Apartment apartment) {
-        apartment.squareFootage += 40;
+    public void upgrade(Studio studio) {
+        studio.squareFootage += 40;
+    }
 
-        if (apartment.getClass() != Studio.class)
-            apartment.numberOfBedrooms += 1;
+    public void upgrade(PenthouseSuite penthouse) {
+        penthouse.squareFootage += 40;
+        penthouse.numberOfBedrooms += 1;
     }
 }
