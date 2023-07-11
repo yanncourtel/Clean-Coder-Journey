60df2578c7d64f1a947b07ca64214d557cff607d
Yann Courtel <courtel.yann@gmail.com>
2023-07-11 18:43:42 +0200

    add racing cars

---
+++ 6-Code-Review/Racing-Cars/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -0,0 +1,26 @@
+package tddmicroexercises.tirepressuremonitoringsystem;
+
+public class Alarm
+{
+    private final double LowPressureThreshold = 17;
+    private final double HighPressureThreshold = 21;
+
+    protected Sensor sensor = new Sensor();
+
+    protected boolean alarmOn = false;
+
+    public void check()
+    {
+        double psiPressureValue = sensor.popNextPressurePsiValue();
+
+        if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
+        {
+            alarmOn = true;
+        }
+    }
+
+    public boolean isAlarmOn()
+    {
+        return alarmOn; 
+    }
+}
