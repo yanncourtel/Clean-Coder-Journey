3689e70a558b03b0d1930737847105c0e25604be
thomas.huard <thomas.huard@its4u-group.com>
2023-08-29 17:11:29 +0200

    Add CSV Parameterized Test for movingForward

--- mars-rover-kata/mars-rover-kata/src/test/java/mars/project/rover/RoverTest.java
+++ mars-rover-kata/mars-rover-kata/src/test/java/mars/project/rover/RoverTest.java
@@ -1,10 +1,10 @@
 package mars.project.rover;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RoverTest {
 
@@ -16,24 +16,14 @@ public class RoverTest {
         assertEquals('N', rover.getDirection(), "should be N");
     }
 
-    @Test
-    void shouldMoveForwardWhenReceivingCommandFAndFacingNorth() {
-        Rover rover = new Rover(3, 4, 'N');
-        rover.receiveCommand("F");
-
-        assertEquals(3, rover.getX(), "should be 3");
-        assertEquals(5, rover.getY(), "should be 5");
-        assertEquals('N', rover.getDirection(), "should be N");
-    }
-
     @ParameterizedTest
-    @ValueSource(chars = {'S'})
-    void shouldMoveForwardWhenReceivingCommandFAndFacingSouth(char direction) {
-        Rover rover = new Rover(3, 4, direction);
+    @CsvSource({"S,3,4,3,3", "N,3,4,3,5", "W,3,4,2,4"})
+    void shouldMoveForwardWhenReceivingCommandF(char direction, int initX, int initY, int targetX, int targetY) {
+        Rover rover = new Rover(initX, initY, direction);
         rover.receiveCommand("F");
 
-        assertEquals(3, rover.getX(), "should be 3");
-        assertEquals(3, rover.getY(), "should be 3");
+        assertEquals(targetX, rover.getX(), "should be " + targetX);
+        assertEquals(targetY, rover.getY(), "should be " + targetY);
         assertEquals(direction, rover.getDirection());
     }
 }
