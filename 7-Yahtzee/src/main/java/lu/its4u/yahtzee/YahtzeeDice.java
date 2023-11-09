a2b059d7689e2246644bfdec7997b9dc1d4196d5
Gaétan Perrein <gaetan.perrein@its4u-group.com>
2023-11-09 16:17:16 +0100

    add test for roll 5 dice and check result

--- 7-Yahtzee/src/main/java/lu/its4u/yahtzee/YahtzeeDice.java
+++ 7-Yahtzee/src/main/java/lu/its4u/yahtzee/YahtzeeDice.java
@@ -1,4 +1,15 @@
 package lu.its4u.yahtzee;
 
 public class YahtzeeDice extends Dice{
+
+    boolean kept;
+    int index;
+
+    public void keep(){
+        kept=true;
+    }
+
+    public void replay(){
+        kept=false;
+    }
 }
