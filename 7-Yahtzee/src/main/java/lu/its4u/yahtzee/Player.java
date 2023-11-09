8ea4a14dcf90edde1661c35397c38bb905217b86
thomas.huard <thomas.huard@its4u-group.com>
2023-11-09 16:03:11 +0100

    Base player definition

--- 7-Yahtzee/src/main/java/lu/its4u/yahtzee/Player.java
+++ 7-Yahtzee/src/main/java/lu/its4u/yahtzee/Player.java
@@ -1,8 +1,22 @@
 package lu.its4u.yahtzee;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Player {
     Set<YahtzeeDice> dices;
-    
+
+    public void keepDice(int index) {
+
+    }
+
+    public void replayDice(int index) {
+
+    }
+
+    public Map<Integer, Boolean> displayDices() {
+        return new HashMap<>();
+    }
+
 }
---
+++ 7-Yahtzee/src/test/java/lu/its4u/yahtzee/PlayerTest.java
@@ -0,0 +1,10 @@
+package lu.its4u.yahtzee;
+
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void test
+}
