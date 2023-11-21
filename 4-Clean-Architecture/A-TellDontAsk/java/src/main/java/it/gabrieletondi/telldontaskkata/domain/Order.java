d96402b88c68bb8c17fc6b745f4af4c0badd1b48
thomas.huard <thomas.huard@its4u-group.com>
2023-11-21 18:12:24 +0100

    Move add item from useCase to Order

--- 4-Clean-Architecture/A-TellDontAsk/java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
+++ 4-Clean-Architecture/A-TellDontAsk/java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
@@ -3,10 +3,14 @@ package it.gabrieletondi.telldontaskkata.domain;
 import it.gabrieletondi.telldontaskkata.useCase.ApprovedOrderCannotBeRejectedException;
 import it.gabrieletondi.telldontaskkata.useCase.RejectedOrderCannotBeApprovedException;
 import it.gabrieletondi.telldontaskkata.useCase.ShippedOrdersCannotBeChangedException;
+import it.gabrieletondi.telldontaskkata.useCase.UnknownProductException;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.math.BigDecimal.valueOf;
+import static java.math.RoundingMode.HALF_UP;
 
 public class Order {
     private BigDecimal total = new BigDecimal("0.00");
@@ -92,5 +96,27 @@ public class Order {
         order.setStatus(OrderStatus.CREATED);
         order.setCurrency("EUR");
         return order;
+    }
+    public void addItem(Product product, int quantity) {
+
+        if (product == null) {
+            throw new UnknownProductException();
+        }
+        else {
+            final BigDecimal unitaryTax = product.getPrice().divide(valueOf(100)).multiply(product.getCategory().getTaxPercentage()).setScale(2, HALF_UP);
+            final BigDecimal unitaryTaxedAmount = product.getPrice().add(unitaryTax).setScale(2, HALF_UP);
+            final BigDecimal taxedAmount = unitaryTaxedAmount.multiply(BigDecimal.valueOf(quantity)).setScale(2, HALF_UP);
+            final BigDecimal taxAmount = unitaryTax.multiply(BigDecimal.valueOf(quantity));
+
+            final OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(product);
+            orderItem.setQuantity(quantity);
+            orderItem.setTax(taxAmount);
+            orderItem.setTaxedAmount(taxedAmount);
+            this.items.add(orderItem);
+            this.setTotal(this.getTotal().add(taxedAmount));
+            this.setTax(this.getTax().add(taxAmount));
+        }
+
     }
 }
--- 4-Clean-Architecture/A-TellDontAsk/java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
+++ 4-Clean-Architecture/A-TellDontAsk/java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
@@ -1,12 +1,6 @@
 package it.gabrieletondi.telldontaskkata.useCase;
 
-import static java.math.BigDecimal.valueOf;
-import static java.math.RoundingMode.HALF_UP;
-
-import java.math.BigDecimal;
-
 import it.gabrieletondi.telldontaskkata.domain.Order;
-import it.gabrieletondi.telldontaskkata.domain.OrderItem;
 import it.gabrieletondi.telldontaskkata.domain.Product;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
 import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
@@ -26,25 +20,7 @@ public class OrderCreationUseCase {
         for (SellItemRequest itemRequest : request.getRequests()) {
             Product product = productCatalog.getByName(itemRequest.getProductName());
 
-            if (product == null) {
-                throw new UnknownProductException();
-            }
-            else {
-                final BigDecimal unitaryTax = product.getPrice().divide(valueOf(100)).multiply(product.getCategory().getTaxPercentage()).setScale(2, HALF_UP);
-                final BigDecimal unitaryTaxedAmount = product.getPrice().add(unitaryTax).setScale(2, HALF_UP);
-                final BigDecimal taxedAmount = unitaryTaxedAmount.multiply(BigDecimal.valueOf(itemRequest.getQuantity())).setScale(2, HALF_UP);
-                final BigDecimal taxAmount = unitaryTax.multiply(BigDecimal.valueOf(itemRequest.getQuantity()));
-
-                final OrderItem orderItem = new OrderItem();
-                orderItem.setProduct(product);
-                orderItem.setQuantity(itemRequest.getQuantity());
-                orderItem.setTax(taxAmount);
-                orderItem.setTaxedAmount(taxedAmount);
-                order.getItems().add(orderItem);
-
-                order.setTotal(order.getTotal().add(taxedAmount));
-                order.setTax(order.getTax().add(taxAmount));
-            }
+            order.addItem(product, itemRequest.getQuantity());
         }
 
         orderRepository.save(order);
